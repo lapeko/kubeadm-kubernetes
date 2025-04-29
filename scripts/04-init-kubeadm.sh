301da1f35f9bf4b785aba2e1a462ba6acd885c33
lapeko <lapeko88@gmail.com>
2025-04-29 23:06:44 +0200

    fix kubeadm initialization

--- scripts/04-init-kubeadm.sh
+++ scripts/04-init-kubeadm.sh
@@ -6,5 +6,15 @@ source "$SCRIPT_DIR/vars.sh"
 cd "$VAGRANT_PATH" || exit 1
 
 echo "initialize kubeadm on $MASTER_NODE..."
-vagrant ssh "$NODE" -c 'sudo kubeadm init --apiserver-advertise-address 192.168.56.11 --pod-network-cidr "10.244.0.0/16" --upload-certs' > /dev/null
+vagrant ssh "$MASTER_NODE" -c "
+  sudo kubeadm init \
+    --apiserver-advertise-address=192.168.56.11 \
+    --pod-network-cidr=10.244.0.0/16 \
+    --upload-certs &&
+  mkdir -p \$HOME/.kube &&
+  sudo cp -i /etc/kubernetes/admin.conf \$HOME/.kube/config &&
+  sudo chown \$(id -u):\$(id -g) \$HOME/.kube/config
+" > /dev/null
 echo "initialize kubeadm on $MASTER_NODE Done"
+
+
