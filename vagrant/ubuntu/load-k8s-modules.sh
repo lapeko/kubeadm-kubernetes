b4c217aff9ccd9c45cb2ade0ffcf1dba15017db3
lapeko <lapeko88@gmail.com>
2025-04-30 09:24:53 +0200

    move IPv4 forwarding setting into load-k8s-modules.sh

--- vagrant/ubuntu/load-k8s-modules.sh
+++ vagrant/ubuntu/load-k8s-modules.sh
@@ -2,17 +2,21 @@
 
 set -e
 
-# Load br_netfilter
-modprobe br_netfilter
+# Enable IP forwarding immediately (runtime)
+sysctl -w net.ipv4.ip_forward=1
 
-# Ensure it loads on boot
-echo "br_netfilter" > /etc/modules-load.d/k8s.conf
-
-# Set sysctl params
+# Ensure all required settings persist after reboot
 cat <<EOF | tee /etc/sysctl.d/k8s.conf
+net.ipv4.ip_forward = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 EOF
 
-# Apply sysctl params without reboot
+# Load kernel module immediately
+modprobe br_netfilter
+
+# Ensure module loads on boot
+echo "br_netfilter" > /etc/modules-load.d/k8s.conf
+
+# Apply sysctl params now
 sysctl --system
