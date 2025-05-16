e4aaea07b6604287c22746ad6f2e6e4284bfa909
lapeko <lapeko88@gmail.com>
2025-05-16 15:44:36 +0200

    copy kubeconfig from controlplane to the host

--- scripts/07-set-kubeadm-context-var.sh
+++ scripts/07-set-kubeadm-context-var.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-echo "To use correct kubeconfig run the commands bellow:
-export KUBECONFIG=$HOME/.kube/kubeadm.conf
-kubectl config --kubeconfig=$HOME/.kube/kubeadm.conf use-context kubernetes-admin@kubernetes"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "$SCRIPT_DIR/vars.sh"
+
+CONF_PATH=$HOME/.kube/kubeadm.config
+
+cd "$VAGRANT_PATH" || exit 1
+
+echo "Copying kubeconfig from \"$MASTER_NODE\" to host..."
+vagrant ssh "$MASTER_NODE" -c "cat /home/vagrant/.kube/config" > "$CONF_PATH"
+echo -e "Kubeconfig saved to $CONF_PATH\n"
+
+echo "To use correct kubeconfig run the commands below on your host:"
+echo "export KUBECONFIG=$CONF_PATH"
+echo "kubectl config --kubeconfig=$CONF_PATH use-context kubernetes-admin@kubernetes"
+echo
