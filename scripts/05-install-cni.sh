e71aff347ffebbf98a655e3fb8173020aeca4a62
lapeko <lapeko88@gmail.com>
2025-05-16 15:43:23 +0200

    fix: enable cross-node pod communication via Flannel CNI

--- scripts/05-install-cni.sh
+++ scripts/05-install-cni.sh
@@ -5,6 +5,16 @@ source "$SCRIPT_DIR/vars.sh"
 
 cd "$VAGRANT_PATH" || exit 1
 
+FLANNEL_MANIFEST=kube-flannel.yml
+
 echo "Deploying Flannel with kubectl on $MASTER_NODE..."
-vagrant ssh "$MASTER_NODE" -c "kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml" > /dev/null
+
+vagrant ssh "$MASTER_NODE" -c "
+  curl -LO https://github.com/flannel-io/flannel/releases/latest/download/${FLANNEL_MANIFEST} &&
+  sed -i '/--kube-subnet-mgr/a\        - --public-ip=\$(FLANNEL_PUBLIC_IP)' ${FLANNEL_MANIFEST} &&
+  sed -i '/env:/a\        - name: FLANNEL_PUBLIC_IP\\n          valueFrom:\\n            fieldRef:\\n              fieldPath: status.hostIP' ${FLANNEL_MANIFEST} &&
+  kubectl apply -f ${FLANNEL_MANIFEST} &&
+  rm ${FLANNEL_MANIFEST}
+" > /dev/null
+
 echo "Deploying Flannel with kubectl on $MASTER_NODE Done"
